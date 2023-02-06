9ab1d06449735890977346c60557d3a0d7426ef1
Atharv Vani <atharvavani110@gmail.com>
2023-02-06 14:31:56 +0530

    25 years logo added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import introLogo from "../images/logo.svg";
 import uniLogo from "../images/medicaps_logo.png"
 import clubLogo from "../images/sahityik_logo.png"
+import years25 from "../images/Logo_25.png"
 import Heading from "./Heading";
 
 export default function Home() {
@@ -17,10 +18,13 @@ export default function Home() {
         <h2>Presented by</h2>
         {/* <h3>Literary Club of Medi-Caps University, Indore</h3> */}
         <div className="img-container">
-          <div>
+          <div className="presented-logo">
             <img src={uniLogo} alt="medicaps-logo" />
           </div>
-          <div>
+          <div className="year-logo">
+            <img src={years25} alt="24-years-logo" />
+          </div>
+          <div className="presented-logo">
           <img src={clubLogo} alt="club-logo" />
           </div>
         </div>
