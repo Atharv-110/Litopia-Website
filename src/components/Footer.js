d9b5adacd7d336408cbbbe1695e60bf603c9b268
Atharv Vani <atharvavani110@gmail.com>
2024-04-15 23:09:27 +0530

    fix: credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,25 +11,13 @@ export default function Footer() {
             <i className="heart bx bxs-heart"></i>
           </h3>
           <p className="credits">
-            Developed by{" "}
+            Designed & Developed by{" "}
             <a
               target="_blank"
               rel="noreferrer"
               href="https://github.com/Atharv-110"
             >
               Atharv
-            </a>
-          </p>
-          <p className="credits">
-            Designed by{" "}
-            <a target="_blank" rel="noreferrer" href="https://github.com/Aadarsh-Mourya">
-              Aadarsh
-            </a>{" "}
-            <a target="_blank" rel="noreferrer" href="https://github.com/Pyra06">
-              Aniruddha
-            </a>{" "}
-            <a target="_blank" rel="noreferrer" href="https://github.com/AryaaSharma">
-              Aryaa
             </a>
           </p>
         </div>
